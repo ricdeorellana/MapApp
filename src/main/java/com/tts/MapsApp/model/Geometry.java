b0d0b8f0f031b88f765cf62878011e7366de812f
ric <ptf119@my.utsa.edu>
2020-07-31 10:36:43 -0500

    Initial Commit

---
+++ src/main/java/com/tts/MapsApp/model/Geometry.java
@@ -0,0 +1,18 @@
+package com.tts.MapsApp.model;
+
+public class Geometry {
+
+	private Location location;
+
+	
+	
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	
+}
---
+++ src/main/resources/static/custom.js
@@ -0,0 +1,32 @@
+let map; 
+
+
+			
+			
+
+function initMap(){
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: coords,
+		zoom: 10, 
+	});
+	let marker = new google.maps.Marker({
+		position: coords,
+		map: map, 
+	
+		animation: google.maps.Animation.BOUNCE, 
+	})
+	
+	
+	
+	let contentString = '<h2>' + city + ', ' + state + '</h2>';
+
+	
+	let infowindow = new google.maps.InfoWindow({ 
+	content: contentString, 
+	});
+	
+	google.maps.event.addListener(marker, 'click', function () {
+	infowindow.open(map, marker);
+});
+}
